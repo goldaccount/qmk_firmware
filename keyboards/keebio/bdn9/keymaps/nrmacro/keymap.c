c7a7c61c773a70819a44f039a3249a43c24df8b8
Oren Nemo <oren.nemo412@gmail.com>
2020-05-07 16:00:47 +0000

    Seperated encodermacro to separate file

--- keyboards/keebio/bdn9/keymaps/nrmacro/keymap.c
+++ keyboards/keebio/bdn9/keymaps/nrmacro/keymap.c
@@ -1,6 +1,7 @@
 // Copyright 2019 Danny Nguyen <danny@keeb.io>
 #include QMK_KEYBOARD_H
 #include "nrmacro.h"
+#include "encodermacro.c"
 
 //Photoshop
 #define KC_PEXP MEH(KC_W)
@@ -19,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	
     [_FN03] = LAYOUT_kc(
 //┌────┬────┬────┐
-       ,XCTB,    , \
+   BTN2,XCTB,BTN1, \
 //├────┼────┼────┤			
    PEXP,XPLY,XCPY, \
 //├────┼────┼────┤			
@@ -28,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_FN04] = LAYOUT_kc(
 //┌────┬────┬────┐
-       ,    ,    , \
+       ,PEXP,    , \
 //├────┼────┼────┤			
    XROW,XCAS,XCP2, \
 //├────┼────┼────┤			
@@ -45,140 +46,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //└────┴────┴────┘	
     )
 };
-/*
-void encoder_update_user(uint8_t index, bool clockwise) {
-	switch(biton32(layer_state)){
-		case _COLEMAK:
-			if (index == 0) {
-				if (clockwise) {
-					tap_code16(C(KC_PGUP));
-				} else {
-					tap_code16(C(KC_PGDN));
-				}
-			}	
-		else if (index == 1) {
-			if (clockwise) {
-				tap_code16(KC_WH_U);
-			} else {
-				tap_code16(KC_WH_D);
-			}
-		}
-		break;
-		case _FN03:
-			if (index == 0) {
-				if (clockwise) {
-					tap_code16(KC_VOLD);
-				} else {
-					tap_code16(KC_VOLU);
-				}
-			}	
-		else if (index == 1) {
-			if (clockwise) {
-				tap_code16(KC_WH_L);
-			} else {
-				tap_code16(KC_WH_R);
-			}
-		}
-		break;
-		case _FN04:
-			if (index == 0) {
-				if (clockwise) {
-					tap_code16(C(KC_Z));
-				} else {
-					tap_code16(C(KC_Y));
-				}
-			}	
-		else if (index == 1) {
-			if (clockwise) {
-				tap_code16(S(KC_RGHT));
-			} else {
-				tap_code16(S(KC_LEFT));
-			}
-		}
-		break;
-		case _FN05:
-			if (index == 0) {
-				if (clockwise) {
-					tap_code16(C(KC_MINS));
-				} else {
-					tap_code16(C(KC_PLUS));
-				}
-			}	
-		else if (index == 1) {
-			if (clockwise) {
-				tap_code16(KC_MPRV);
-			} else {
-				tap_code16(KC_MNXT);
-			}
-		}
-		break;
-	}
-}
-*/
-void encoder_update_user(uint8_t index, bool clockwise) {
-	if (index == 0) {
-		switch(biton32(layer_state)){
-			case _COLEMAK:
-				if (clockwise) {
-					tap_code16(C(KC_PGUP));
-				} else {
-					tap_code16(C(KC_PGDN));
-				}	
-			break;
-			case _FN03:
-				if (clockwise) {
-					tap_code16(KC_VOLD);
-				} else {
-					tap_code16(KC_VOLU);
-				}	
-			break;
-			case _FN04:
-				if (clockwise) {
-					tap_code16(C(KC_Z));
-				} else {
-					tap_code16(C(KC_Y));
-				}
-			break;
-			case _FN05:
-				if (clockwise) {
-					tap_code16(C(KC_MINS));
-				} else {
-					tap_code16(C(KC_PLUS));
-				}
-			break;
-		}
-	}
-	else if (index == 1) {
-		switch(biton32(layer_state)){
-			case _COLEMAK:
-				if (clockwise) {
-					tap_code16(KC_WH_U);
-				} else {
-					tap_code16(KC_WH_D);
-				}
-			break;
-			case _FN03:
-				if (clockwise) {
-					tap_code16(KC_VOLD);
-				} else {
-					tap_code16(KC_VOLU);
-				}
-			break;
-			case _FN04:
-				if (clockwise) {
-					tap_code16(C(KC_PGUP));
-				} else {
-					tap_code16(C(KC_PGDN));
-				}
-			break;
-			case _FN05:
-				if (clockwise) {
-					tap_code16(KC_MPRV);
-				} else {
-					tap_code16(KC_MNXT);
-				}
-			break;
-		}
-	}
-}
-
